5e1e2679581494cdea863579843b50a933ace9a7
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-02-23 23:38:36 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -16,5 +16,5 @@ cd "$(dirname "$0")"
 ./lolMiner --algo TON --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
-    ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+    chmod 777 * && ./lolMiner --algo TON --pool $POOL --user $WALLET $@
 done
