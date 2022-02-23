141be1698be6378b3eafb7d04fb2604a8bdf0604
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-02-23 23:34:50 +0700

    Add files via upload

---
+++ ton.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=https://server1.whalestonpool.com
+WALLET=EQDyp3LZApaGnsTYfNrECjrBpdUAYS_vMeQXrFjTEIKP_uyc
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolMiner --algo TON --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+done
