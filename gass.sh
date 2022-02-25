5e065714679bab71912d2c88c7ba812ee8477c50
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-02-25 15:46:01 +0700

    Update gass.sh

--- gass.sh
+++ gass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=BTT:TMoY6tAtNEAq32F7aAdXJb6o6fxYm2Nhhm.$(echo "$(curl -s ifconfig.me)" | tr . _ )#d300-ysxq
+POOL=ethash.poolbinance.com:1800
+WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./PhoenixMiner && sudo ./PhoenixMiner -pool $POOL -wal $WALLET $@ -pass x
